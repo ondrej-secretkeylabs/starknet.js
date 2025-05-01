7a0a1af7708aa4380eec6495b9debcb40ee2d00e
Ondřej Sojka <ondrej@secretkeylabs.com>
2025-05-01 12:58:07 +0200

    fix: failed to execute 'fetch' on 'Window': Illegal invocation

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,6 +1,6 @@
 import { LibraryError } from './errors';
 
-export default (typeof fetch === 'function' && fetch) ||
+export default (typeof fetch === 'function' && window.fetch.bind(window)) ||
   // throw with instructions when no fetch is detected
   ((() => {
     throw new LibraryError(
